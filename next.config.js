70012ff9df09033927f7c5b12012f2763f3ff32d
BNsrujan <srujanbn12@gmail.com>
2025-07-04 10:40:39 +0530

    all set

---
+++ next.config.js
@@ -0,0 +1,29 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  // Add configuration for static files
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+    ]
+    
+  },
+}
+
+module.exports = nextConfig 
